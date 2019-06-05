7232307aaa6af130536198d2d3a41092aa213a1f
Benji Shankwitz <bshankwitz@gmail.com>
2019-06-05 16:22:51 -0400

    test4

--- farmers/scripts.js
+++ farmers/scripts.js
@@ -63,9 +63,9 @@ function initClient() {
     'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
   }).then(function() {
   	// console.log(gapi)
-    gapi.auth2.getAuthInstance().signIn();
-    // gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-    // updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    // gapi.auth2.getAuthInstance().signIn();
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
   });
 }
 
